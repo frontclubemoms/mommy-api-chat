9eed0075cc67b34c9ccc18541981305417e5f3e1
frontclubemoms <frontend@clubemoms.com.br>
2025-08-13 09:54:18 -0300

    redeploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from Mommy API Chat!';
+    return 'Hello World from Mommy API Chat!!';
   }
 }
